97a0f339fe9dffbfaeedea28ab5a922c8ef17840
tushar-mandge <aakashmandge2002@gmail.com>
2025-11-18 11:17:26 +0530

    Enhance math tests with sub and multi functions
    Added subtraction and multiplication tests, and corrected an addition test expectation.

--- math.test.js
+++ math.test.js
@@ -1,9 +1,21 @@
-const { add } = require('./math');
+const { add, sub , multi } = require('./math');
 
 test('2 + 3 should be 5', () => {
   expect(add(2, 3)).toBe(5);
 });
 
-test('10 + 20 should be 25', () => {
-  expect(add(10, 20)).toBe(25);
+test('10 + 20 should be 30', () => {
+  expect(add(10, 20)).toBe(30);
 });
+
+test('2 - 3 should be -1', () => {
+  expect(sub(2, 3)).toBe(-1);
+});
+
+test('2 * 3 should be 9', () => {
+  except(multi(2,3)).toBe(9);
+})
+
+test('3 * 3 should be 9', () => {
+  except(multi(3,3)).toBe(9);
+})
