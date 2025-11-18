590aa5d25e0725fd4ef4db59d64f3671698d6ef8
tushar-mandge <aakashmandge2002@gmail.com>
2025-11-18 11:06:26 +0530

    Add tests for add function in math.test.js

---
+++ math.test.js
@@ -0,0 +1,9 @@
+const { add } = require('./math');
+
+test('2 + 3 should be 5', () => {
+  expect(add(2, 3)).toBe(5);
+});
+
+test('10 + 20 should be 30', () => {
+  expect(add(10, 20)).toBe(30);
+});
