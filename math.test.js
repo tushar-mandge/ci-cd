b9bad4f2919bb879e1284f12c480da78ea158c87
tushar-mandge <aakashmandge2002@gmail.com>
2025-11-18 11:12:36 +0530

    version v2

--- math.test.js
+++ math.test.js
@@ -4,6 +4,6 @@ test('2 + 3 should be 5', () => {
   expect(add(2, 3)).toBe(5);
 });
 
-test('10 + 20 should be 30', () => {
-  expect(add(10, 20)).toBe(30);
+test('10 + 20 should be 25', () => {
+  expect(add(10, 20)).toBe(25);
 });
